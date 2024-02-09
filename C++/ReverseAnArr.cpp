e243158e7ff196f59c6f3a5e0c1b133ab2d6fa55
AMITH N <117341739+amithshetty0906@users.noreply.github.com>
2024-02-09 16:30:21 +0000

    reverse

---
+++ C++/ReverseAnArr.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void reverseArray(int arr[], int size) {
+    cout << "Reversed array: ";
+    for (int i = size - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter " << size << " elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    reverseArray(arr, size);
+
+    return 0;
+}
