f93a11f8534ad32349db4a01e7d9d48c19d3fe77
AMITH N <117341739+amithshetty0906@users.noreply.github.com>
2024-03-07 09:33:35 +0000

    OMp

--- MPO/Omp/p6.c
+++ MPO/Omp/p6.c
@@ -10,9 +10,9 @@ const int COLS =3;
 
 void transpose_parallel(int input[ROWS][COLS], int output[COLS][ROWS]) {
     #pragma omp parallel for
-    
+
     for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
+        for (int j = 0; j < COLS; j++){
             output[j][i] = input[i][j];
         }
     }
